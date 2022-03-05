a54c0407c990ffad55c077deaa769f3231dad237
Soumik Sarkar <ssarkar12398@gmail.com>
2022-03-05 19:57:41 +0530

    final commit

--- middleware.js
+++ middleware.js
@@ -12,6 +12,14 @@ module.exports.isLoggedIn = (req, res, next)=>{
     next();
 }
 
+module.exports.alreadyLoggedIn = (req, res, next)=>{
+    if(req.isAuthenticated()){
+        return res.redirect('/gyms');
+    }
+    next();
+}
+
+
 module.exports.validateGym = (req , res , next) =>{ 
     const {error} = gymSchema.validate(req.body)
     if(error){
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router();
-const passport = require('passport')
-const users = require('../controllers/users')
-const catchAsync = require('../utils/catchAsync')
+const passport = require('passport');
+const users = require('../controllers/users');
+const catchAsync = require('../utils/catchAsync');
+const { alreadyLoggedIn } = require('../middleware')
 
 
-router.get('/register', users.renderRegister);
+router.get('/register',alreadyLoggedIn, users.renderRegister);
 
 router.post('/register', catchAsync(users.register));
 
-router.get('/login', users.renderLogin)
+router.get('/login',alreadyLoggedIn, users.renderLogin)
 
 router.post('/login',passport.authenticate('local',{failureFlash: true, failureRedirect:'/login'}), users.login)
 
